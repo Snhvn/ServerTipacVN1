e8405fc7041c9fa12e396cacfb0bbc9a2db55063
Snhvn <snhvnnguyen@gmail.com>
2025-06-04 21:47:25 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,4 +2,5 @@ echo "Starting PufferPanel service..."
 systemctl enable --now pufferpanel
 pufferpanel user add --admin --email "admin@servertipacvn.com" --name "Admin" --password "dsc.gg/servertipacvn"
 echo "Starting PufferPanel service..."
+sed -i "s/\"host\": \"0.0.0.0:8080\"/\"host\": \"0.0.0.0:8080\"/g" /etc/pufferpanel/config.json
 systemctl restart pufferpanel
